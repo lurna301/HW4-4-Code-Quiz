2b0db3e712fa6e4cdd858958704597d1e83872e8
Lauren A <laurenagraphicdesign@gmail.com>
2022-04-15 01:47:07 -0400

    some HTML, CSS & JS, set up

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var test = 26;
+
+var start = document.querySelector("#startButton");
+var quizOptions = document.querySelector("#quizOptionContainer");
+var answerOne = document.querySelector("#quizAnswerOption1");
+var answerTwo = document.querySelector("#quizAnswerOption2");
+var answerThree = document.querySelector("#quizAnswerOption3");
+var answerFour = document.querySelector("#quizAnswerOption4");
