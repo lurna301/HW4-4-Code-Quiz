bc29689222a618eb96104befe4ba376cf809313a
Lauren Anderson <laurenagraphicdesign@gmail.com>
2022-05-17 22:13:25 -0400

    hide toggle works; questions now show up w/ for loop

--- assets/script.js
+++ assets/script.js
@@ -8,18 +8,52 @@ var answerOne = document.querySelector("#quizAnswerOption1");
 var answerTwo = document.querySelector("#quizAnswerOption2");
 var answerThree = document.querySelector("#quizAnswerOption3");
 var answerFour = document.querySelector("#quizAnswerOption4");
-
+var quizShow = document.querySelector("#quizCard");
 var secondsLeft = 80;
+// Create an  array of questions
+var questions = [
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts",
+  },
+  {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses",
+  },
+  {
+      title: "There are 3 different ways in which a JavaScript code can be involved in an HTML file. Selct the one that's not correct.",
+      choices: ["Inline", "Import", "External", "Internal"],
+      answer: "Import",
+  },
+  {
+      title: "How to create an array in js ?",
+      choices: ["var A[]=", "var A{}=", "var A=[]", "var A={}"],
+
+      answer: "var A=[]",
+  },
+  {   
+      title: "HTML element that can be accessed in a Javascript code: Chose the one that will return an array of elements",
+      choices: ["getElementById(‘idname’)", "getElementsByClass(‘classname’)", 
+      "getElementsByTagName(‘tagname’)", "querySelectorAll()"],
+      answer: "querySelectorAll()",
+  }
+]
+
 
 
 // Start button trigger the first question and next button to display
 startButton.addEventListener("click", function(){
     
     setTime();
+    quizShow.classList.remove("hide");
+    
+
 });
 // nextButton.addEventListener("click", () => {
 //     quizOptions++
-//  setNextQuestion()
+//  setNextQuestion() 
 // });
 
 function setTime() {
@@ -34,4 +68,24 @@ function setTime() {
     }, 1000);
   }
   
+// show the question
 
+var questionEl = document.querySelector("#quizQuestion");
+function showQuestions(){
+
+
+ 
+  for (var i = 0; i < questions.length; i++){
+
+    questionEl.textContent = questions[i].title;
+
+  }  
+
+
+ 
+ 
+ 
+
+};
+
+showQuestions();
